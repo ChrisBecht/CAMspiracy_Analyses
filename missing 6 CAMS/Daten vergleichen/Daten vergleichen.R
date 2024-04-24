701c37c676caacfe962b2fa22f76c39b8852f398
ChrisBecht <christophe.be@t-online.de>
2024-04-24 15:35:46 +0200

    Some updates for Leiden.

--- missing 6 CAMS/Daten vergleichen/Daten vergleichen.R
+++ missing 6 CAMS/Daten vergleichen/Daten vergleichen.R
@@ -77,3 +77,10 @@ write.xlsx(t2_missing, file = "t2_missing.xlsx")
 setwd("..")
 
 # -> 8 VPn wurden eingeladen, haben CAMs gezeichnet, aber keine Daten für t2. 
+# -> 1 Person zusätzlich entfernt wegen fehlender Werte
+
+#####################
+
+# in t2 wurden insg. 130 CAMs erhoben (DE: 75, USA: 55)
+# in zwei CAMs wurden die Valenzen nicht instruktionsgemäß angepasst (5b7e118e87d85f0001baac27, 5ff5922c4721d5000981fcde)
+# in 7 CAMs gab es technische Probleme, z.B. Neuladen des Browsers (5fff542aeed8c2065133114e, 5c395df5f5ebd50001850900, 63d13f2f59ef764f641406dd, 61188057fe5a395476806c12, 60c14a3e834adb62102cffa0, 62d59a066ee263bb659aa445, 63cedc8a97a0bb604736baea)
